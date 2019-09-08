a3450ff535a6c76021c7ed720cec533e2d3d9139
Cristian Salamea <ovnicraft@iMac-de-Cristian.local>
2019-09-07 22:28:13 -0500

    ejemplo python

---
+++ PYTHON/envia_ec.py
@@ -0,0 +1,53 @@
+import requests
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+GRANT_TYPE = "client_credentials"
+ENDPOINT_AUTH = "https://api.login-sms.com/token"
+ENDPOINT_SEND_SMS = "https://api.login-sms.com/messages/send"
+
+
+auth = requests.post(
+    ENDPOINT_AUTH,
+    data={
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": GRANT_TYPE,
+    },
+)
+
+if auth.ok:
+    access = auth.json()
+    print("access token granted")
+
+
+def get_token():
+    auth = requests.post(
+        ENDPOINT_AUTH,
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "grant_type": GRANT_TYPE,
+        },
+    )
+    return auth
+
+
+def send_sms(access, number, message):
+    headers = {
+        "Authorization": f"{access.get('token_type')} {access.get('access_token')}"
+    }
+    data = {"to_number": number, "content": message}
+
+    res_send = requests.post(ENDPOINT_SEND_SMS, headers=headers, json=data)
+    if res_send.ok:
+        print("mensaje enviado")
+    else:
+        print(res_send.text)
+        print("mensaje no enviado")
+
+
+if __name__ == "__main__":
+    access = get_token()
+    msg = "Seguros Constitucion informa que su póliza contratada registra un saldo pendiente de 1200.00 con 13 días vencidos. Agradecemos su pago inmediato"
+    send_sms(access, "+593999999999", msg)
